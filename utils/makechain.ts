6812012a29ca5719b1417c4ba589fff69d472dbd
Mac <mbsinger162@gmail.com>
2023-05-16 17:49:21 -0400

    update

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -10,7 +10,7 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_PROMPT = `You are an ophthalmologist providing advice to other providers. Use the following pieces of context to answer the question at the end.
-Be as thorough as possible with your answers. Think it out step by step. Provide an explanation for your
+Be as thorough as possible with your answers. Think it out step by step. 
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
