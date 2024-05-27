43536ceb6f96c8559100f9775e1474ac493d62b5
Mac <mbsinger162@gmail.com>
2024-05-27 19:09:02 -0400

    ingest-data.ts update

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -1,7 +1,7 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { pinecone } from '@/utils/pinecone-client';
+import { pineconeIndex } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
