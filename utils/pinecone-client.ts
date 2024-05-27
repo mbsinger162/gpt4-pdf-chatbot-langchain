e7da20aa2cb461c0f6f3300e29768cc0f09d6a41
Mac <mbsinger162@gmail.com>
2024-05-27 18:34:26 -0400

    pincecone-client updates

--- utils/pinecone-client.ts
+++ utils/pinecone-client.ts
@@ -1,3 +1,27 @@
+// import { PineconeClient } from '@pinecone-database/pinecone';
+
+// if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
+//   throw new Error('Pinecone environment or api key vars missing');
+// }
+
+// async function initPinecone() {
+//   try {
+//     const pinecone = new PineconeClient();
+
+//     await pinecone.init({
+//       environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+//       apiKey: process.env.PINECONE_API_KEY ?? '',
+//     });
+
+//     return pinecone;
+//   } catch (error) {
+//     console.log('error', error);
+//     throw new Error('Failed to initialize Pinecone Client');
+//   }
+// }
+
+// export const pinecone = await initPinecone();
+
 import { PineconeClient } from '@pinecone-database/pinecone';
 
 if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
@@ -9,7 +33,7 @@ async function initPinecone() {
     const pinecone = new PineconeClient();
 
     await pinecone.init({
-      environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+      environment: process.env.PINECONE_ENVIRONMENT ?? '',
       apiKey: process.env.PINECONE_API_KEY ?? '',
     });
 
